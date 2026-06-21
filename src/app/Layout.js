cccf9bc51ec44bb101b4b169745d3f4eb271a3c0
casamfelix-spec <casamfelix@gmail.com>
2026-06-21 12:55:40 +0200

    Create Layout.js

---
+++ src/app/Layout.js
@@ -0,0 +1,13 @@
+import './globals.css';
+import Header from '../components/Header';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-[#0a192f] text-slate-100 min-h-screen">
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
