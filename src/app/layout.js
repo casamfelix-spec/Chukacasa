4f92af80b904d13d2a4983843b9e72315a081550
casamfelix-spec <casamfelix@gmail.com>
2026-06-21 16:15:30 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-[#00061C] text-slate-100 min-h-screen">
+      <body className="bg-[#0a192f] text-slate-100 min-h-screen">
         <Header />
         {children}
       </body>
